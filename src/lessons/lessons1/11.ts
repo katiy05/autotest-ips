0a94fb3431dba2f2c83620aca0cd7b757b4ef74c
ekaterina.nikolaeva <ekaterina.nikolaeva@ispring.com>
2023-03-06 14:22:43 +0300

    Update 11.ts

--- src/lessons/lessons1/11.ts
+++ src/lessons/lessons1/11.ts
@@ -1,18 +1,14 @@
-function calc(a: number, b: number, c: string) {
-    if (c === '+') {
-        console.log(a + b)
-    }
-    if (c === '-') {
-        console.log(a - b)
-    }
-    if (c === '*') {
-        console.log(a * b)
-    }
-    if (c === '/' && b != 0) {
-        console.log(a / b)
-    }
-    else if (c === '/' && b === 0) {
-        console.log('Делаить на 0 нельзя')
-    }
+const sum = (a: number, b: number) => a + b
+const difference = (a: number, b: number) => a - b
+const multiplication = (a: number, b: number) => a * b
+const division = (a: number, b: number) => a / b
+
+function calc(a: number, b: number, callback: (a: number, b: number) => number) {
+    // else if (c === '/' && b === 0) {
+    //     console.log('Делаить на 0 нельзя')
+    // }
+    const result: number = callback(a, b)
+    console.log(result)
 }
-calc(5, 0, '/')
+
+calc(5, 7, difference)
